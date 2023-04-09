0f1c3352a44f01f26cb0ba353b1829055636afdc
Alifiansyah <alifiansyahfirdaus@gmail.com>
2023-04-09 23:10:02 +0700

    Adding localstorage feature to keep data after refresh

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const toDoList = [];
+
+let prevToDo = JSON.parse(localStorage.getItem("todos"));
+prevToDo = prevToDo || [];
+
+prevToDo.forEach(element => {
+    const li = document.createElement("li");
+    li.innerHTML = element;
+    document.getElementById("data").appendChild(li);
+});
+
+
 function addToDo(inputData) {
     const listElement = document.createElement("li");
     const textNode = document.createTextNode(inputData);
     listElement.appendChild(textNode);
     document.getElementById("data").appendChild(listElement);
     document.getElementById("todo").value = "";
+
+    toDoList.push(inputData);
+    localStorage.setItem("todos", JSON.stringify(toDoList));
 }
