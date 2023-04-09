f777a3fd71c72c1ba67c6b037bc625da43d463f6
Alifiansyah <alifiansyahfirdaus@gmail.com>
2023-04-09 21:56:01 +0700

    Adding create todo function from input

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function addToDo() {
+    const inputData = document.getElementById("todo").value;
+    const listElement = document.createElement("li");
+    const textNode = document.createTextNode(inputData);
+    listElement.appendChild(textNode);
+    document.getElementById("data").appendChild(listElement);
+    document.getElementById("todo").value = "";
+}
+function tes() {
+    
+}
