50f99f186307b77f44ef751181d6639a37b185d6
Alifiansyah <alifiansyahfirdaus@gmail.com>
2023-04-09 22:15:28 +0700

    Change how function add todo from input

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function addToDo() {
-    const inputData = document.getElementById("todo").value;
+function addToDo(inputData) {
     const listElement = document.createElement("li");
     const textNode = document.createTextNode(inputData);
     listElement.appendChild(textNode);
     document.getElementById("data").appendChild(listElement);
     document.getElementById("todo").value = "";
-}
-function tes() {
-    
 }
